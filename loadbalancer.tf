16f82c1ca4a32113c0330b8fc9743b27363088cd
Nisha Bidave <nisha.bidave@cloudreach.com>
2022-08-04 20:29:42 +0530

    removed the comments

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -4,15 +4,8 @@ resource "aws_lb" "group_3_lb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id]
-  # vpc_id             = data.aws_vpc.vpc_group3.id
 
   enable_deletion_protection = true
-
-#   access_logs {
-#     bucket  = aws_s3_bucket.lb_logs.bucket
-#     prefix  = "test-lb"
-#     enabled = true
-#   }
 
   tags = {
     Environment = "group_3_lb"
@@ -25,26 +18,16 @@ resource "aws_lb_listener" "lb_listener" {
   port              = "80"
   protocol          = "HTTP"
 
-  # default_action {
-  #   type             = "forward"
-  #   target_group_arn = aws_lb_target_group.blue.arn
-  # }
-
-  # default_action {
-  #   type             = "forward"
-  #   target_group_arn = aws_lb_target_group.green.arn
-  # }
-
-    default_action {
-      type             = "forward"
-      forward {
-          target_group {
-              arn = aws_lb_target_group.blue.arn
-          }
-
-          target_group {
-              arn = aws_lb_target_group.green.arn
-          }
+  default_action {
+    type = "forward"
+    forward {
+      target_group {
+        arn = aws_lb_target_group.blue.arn
       }
+
+      target_group {
+        arn = aws_lb_target_group.green.arn
+      }
+    }
   }
 }
